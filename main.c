781a6b3768be6e0bd45add98c836aa715c4a33da
unknown <dmilan.felvi@gmail.com>
2025-09-29 08:46:29 +0200

    1 feladat megoldva.

---
+++ main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct a {
+    double X,Y,sugar;
+    char csillagasz[30];
+} Krater;
+
+typedef Krater kraterek[100];
+
+int feladat01(kraterek tarol) {
+    FILE* be;
+    be = fopen("felszin_tpont.txt","r");
+    char sor[50];
+    int s_sz = 0;
+    if(be == NULL) {
+      printf("Fajkezelesi problema!");
+      return 1;
+    }
+    while (fgets(sor,sizeof(sor),be)&&s_sz < 100) {
+        sor[strcspn(sor,"\n")] = '\0';
+        char* reszlet = strtok(sor,"\t");
+        tarol[s_sz].X = atof(reszlet);
+        reszlet = strtok(NULL,"\t");
+        tarol[s_sz].Y = atof(reszlet);
+        reszlet = strtok(NULL,"\t");
+        tarol[s_sz].sugar = atof(reszlet);
+        reszlet = strtok(NULL,"\t");
+        strcpy(tarol[s_sz].csillagasz,reszlet);
+        s_sz++;
+    }
+    fclose(be);
+    return s_sz;
+}
+int main()
+{
+    kraterek tarol;
+    int sorok_szama = feladat01(tarol);
+    printf("%d\n",sorok_szama);
+    for (int i = 0; i<sorok_szama;i++) {
+         printf("%f %f %f %s\n",tarol[i].X,tarol[i].Y,tarol[i].sugar,tarol[i].csillagasz);
+    }
+    return 0;
+}
