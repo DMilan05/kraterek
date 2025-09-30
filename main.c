053fa1523eda0c1d75fe91a4bf1117463ff6354c
unknown <dmilan.felvi@gmail.com>
2025-09-30 10:59:36 +0200

    6 feladat kb. megoldva.

--- main.c
+++ main.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 typedef struct a {
     double X,Y,sugar;
-    char csillagasz[30];
+    char nev[30];
 } Krater;
 
 typedef Krater kraterek[100];
@@ -27,7 +28,7 @@ int feladat01(kraterek tarol) {
         reszlet = strtok(NULL,"\t");
         tarol[s_sz].sugar = atof(reszlet);
         reszlet = strtok(NULL,"\t");
-        strcpy(tarol[s_sz].csillagasz,reszlet);
+        strcpy(tarol[s_sz].nev,reszlet);
         s_sz++;
     }
     fclose(be);
@@ -41,17 +42,59 @@ void feladat02(int s_sz) {
 
 void feladat03(kraterek tarol,int s_sz) {
     printf("3.feladat:\n");
-    char be_csillagasz[50];
+    char be_nev[30];
     printf("Kerem egy krater nevet: ");
-    scanf("%s",be_csillagasz);
+    scanf("%s",be_nev);
     int talalt = 0;
-    for(int i=0;i<s_sz;i++) {
-        if(strcmp(tarol[i].csillagasz,be_csillagasz) == 0) {
-            printf("A(z) %s kozeppontja X=%.2lf Y=%.2lf sugara R=%.2lf.\n",tarol[i].csillagasz,tarol[i].X,tarol[i].Y,tarol[i].sugar);
+    int i = 0;
+    while(i<s_sz && talalt !=1) {
+
+        if(strcmp(tarol[i].nev,be_nev) == 0 ) {
+            printf("A(z) %s kozeppontja X=%.2lf Y=%.2lf sugara R=%.2lf.\n",tarol[i].nev,tarol[i].X,tarol[i].Y,tarol[i].sugar);
             talalt = 1;
+
+        } else {
+            i++;
         }
     }
-    if(!talalt) printf("Nincs ilyen nevu krater.");
+
+    if(!talalt) printf("Nincs ilyen nevu krater.\n");
+}
+
+void feladat04(kraterek tarol,int s_sz) {
+    int ln_sugar = tarol[0].sugar;
+    int ln_sugar_index = 0;
+    for (int i =0;i<s_sz;i++) {
+        if (tarol[i].sugar > ln_sugar) {
+            ln_sugar = tarol[i].sugar;
+            ln_sugar_index = i;
+        }
+    }
+
+    printf("4.feladat:\n");
+    printf("A legnagyobb krater neve es sugara: %s %.2lf\n",tarol[ln_sugar_index].nev,tarol[ln_sugar_index].sugar);
+
+}
+
+//5. feladat
+double tavolsag(double x1,double x2,double y1,double y2) {
+    return sqrt(((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1)));
+}
+
+void feladat06(kraterek tarol,int s_sz) {
+    printf("6.feladat:\n");
+    char be_nev[30];
+    printf("Kerem egy krater nevet: ");
+    scanf("%s",be_nev);
+    int hol_van = 0;
+    while (strcmp(tarol[hol_van].nev,be_nev)!=0) hol_van++;
+
+    for (int i =0;i<s_sz;i++) {
+        if (tarol[i].sugar > ln_sugar) {
+            ln_sugar = tarol[i].sugar;
+            ln_sugar_index = i;
+        }
+    }
 }
 
 int main()
@@ -60,11 +103,11 @@ int main()
     int sorok_szama = feladat01(tarol);
     /** printf("%d\n",sorok_szama);
     for(int i = 0; i<sorok_szama;i++) {
-         printf("%.2lf %.2lf %.2lf %s\n",tarol[i].X,tarol[i].Y,tarol[i].sugar,tarol[i].csillagasz);
+         printf("%.2lf %.2lf %.2lf %s\n",tarol[i].X,tarol[i].Y,tarol[i].sugar,tarol[i].nev);
     }**/
     feladat02(sorok_szama);
     feladat03(tarol,sorok_szama);
-
+    feladat04(tarol,sorok_szama);
 
 
 
